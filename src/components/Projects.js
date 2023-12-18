7c52573e226195e4977829eb230a3544433e22f7
aviralkaksha <aviraltuteja@gmail.com>
2023-12-18 18:27:39 +0530

    Capture's Landing Page

---
+++ src/components/Projects.js
@@ -0,0 +1,84 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.jpeg";
+import projImg2 from "../assets/img/project-img2.jpeg";
+import projImg3 from "../assets/img/project-img3.jpeg";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "God in a Metro",
+      description:  "A zero budget web-series",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Silsilay",
+      description: "Original Song and Music Video",
+      imgUrl: projImg2,
+    },
+    {
+      title: "NatJio Documentary",
+      description: "A comedy sketch",
+      imgUrl: projImg3,
+    },
+   
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>The Works</h2>
+                <p> Listed below is an array of Projects that we have completed in the past few years.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                      <p>Apart from these projects, we have had multiple corporate clients from all over the country for whom we have made animated as well as shot videos.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>We regularly post content on our Social Media platforms, you can check them out by clicking on the Instagram icon.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
